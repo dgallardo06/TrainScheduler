598442f4103dd852ac53eff44771755e06a30b33
dgallardo06 <denise.gallardo1@gmail.com>
2018-01-17 21:21:53 -0500

    added frequency to HTML, math for minutes away

---
+++ javascript/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyAfwgybvMN5lmq_2cHeBkUrxlYr4ldlUW8",
+	    authDomain: "trainscheduler-gt-dg.firebaseapp.com",
+	    databaseURL: "https://trainscheduler-gt-dg.firebaseio.com",
+	    projectId: "trainscheduler-gt-dg",
+	    storageBucket: "trainscheduler-gt-dg.appspot.com",
+	    messagingSenderId: "153017563398"
+	  };
+	
+	firebase.initializeApp(config);
+
+	var database = firebase.database();
+
+	$("#add-train-btn").on("click", function(event){
+		event.preventDefault();
+
+		var trainName = $("#train-input").val().trim();
+		var destinationName = $("#destination-input").val().trim();
+		var trainTime = moment($("#first-train-input").val().trim(), "HH:mm").format("X");
+		var trainFrequency = $("#frequency-input").val().trim();
+
+
+		var newTrain = {
+			train: trainName,
+			destination: destinationName,
+			time: trainTime,
+			frequency: trainFrequency
+		};
+
+		database.ref().push(newTrain);
+
+		console.log(newTrain.train);
+		console.log(newTrain.destination);
+		console.log(newTrain.time);
+		console.log(newTrain.frequency);
+
+		//********** Create Modal
+		alert("Employee successfully added");
+
+		//Clear text boxes
+		$("#train-input").val("");
+		$("#destination-input").val("");
+		$("#first-train-input").val("");
+		$("#frequency-input").val("");
+
+
+		database.ref().on("child_added", function(childSnapshot){
+
+			console.log(childSnapshot.val());
+
+			//Store everything into a variable
+			var trnName = childSnapshot.val().train;
+			var trnDestination = childSnapshot.val().destination;
+			var trnTime = childSnapshot.val().time;
+			var trnRate = childSnapshot.val().frequency;
+
+			console.log(trnName);
+			console.log(trnDestination);
+			console.log(trnTime);
+			console.log(trnRate);
+
+			//Format time to military time
+			var trnTimeFormatted = moment(trnTime).format("HH");
+
+			//Calculations for next arrival and minutes away
+			var firstTrainConverted = moment(trnTime, "HH").subtract(1,"years");
+
+			var currentTime = moment();
+
+		    var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+
+		    // Time apart (remainder)
+		    var tRemainder = diffTime % trnRate;
+
+		    // Minutes away
+		    var minutesAway = trnRate - tRemainder;
+
+		    // Next arrival
+		    var nextTrain = moment().add(minutesAway, "minutes");
+
+			//Show train info in schedule table
+			$("#train-schedule-table > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDestination + "</td><td>" + trnRate + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td></tr>");
+
+		});
+	});
+
+});
+
